c77a44b8711ae3511657ce9da30255efe0f74a63
Lokesh C <challagulla.l@northeastern.edu>
2025-07-06 15:37:31 -0400

    fix: validation script

--- .github/scripts/validate.sh
+++ .github/scripts/validate.sh
@@ -11,8 +11,8 @@ if [ -z "$CHANGED_FILES" ]; then
 fi
 
 for file in $CHANGED_FILES; do
-  if [ ! -f "$file" ]; then
-    echo "Skipped deleted file: $file"  # Skip deleted files
+  if [ ! -f "$file" ]; then 
+    echo "Skipped deleted file: $file" # Skip deleted files
     continue
   fi
 
@@ -22,22 +22,27 @@ for file in $CHANGED_FILES; do
     name=$(yq e '.metadata.name' "$file")
     chart=$(yq e '.spec.chart.spec.chart' "$file")
     version=$(yq e '.spec.chart.spec.version' "$file")
-    repo=$(yq e '.spec.chart.spec.sourceRef.name' "$file")
     namespace=$(yq e '.spec.targetNamespace // "default"' "$file")
     values_file="$(dirname "$file")/values.yaml"
 
-    if ! helm template "$release_name" "$chart" \
+    if ! helm template "$name" "$chart" \
       --version "$version" \
       --namespace "$namespace" \
       ${values_file:+--values "$values_file"} > /tmp/rendered.yaml; then
-      echo "helm template failed $file"
+      echo "helm template failed for $file"
       exit 1
     fi
 
     kubeconform -strict -ignore-missing-schemas -summary -output text /tmp/rendered.yaml
 
   else
-    echo "Checking raw k8 YAMLs"
-    kubeconform -strict -ignore-missing-schemas -summary -output text "$file"
+    echo "Checking k8 YAMLs"
+    if grep -q 'apiVersion:' "$file" && grep -q 'kind:' "$file"; then
+      kubeconform -strict -ignore-missing-schemas -summary -output text "$file"
+    else
+      echo "Skipping non-Kubernetes YAML: $file"
+    fi
   fi
 done
+
+  
